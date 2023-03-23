cdb604650df66d364031c4f0a4886d2932dc738f
Muskan <muskansri1@gmail.com>
2023-03-23 17:10:16 -0400

    All changes added

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-// require('./config/db');
-// const express=require('express');
-// const bodyparser=require('body-parser');
-// const cookieParser=require('cookie-parser');
-// const bcrypt = require('bcrypt');
-// const UserRouter= require('./apis/user');
-
-// const app=express();
-// app.use(express.json());
-// // app use
-// app.use(bodyparser.urlencoded({extended : false}));
-// app.use(bodyparser.json());
-// app.use(cookieParser());
-
-// // app.get('/',function(req,res){
-// //     res.status(200).send(`Welcome to login , sign-up api`);
-// // });
-
-// app.use('/user', UserRouter)
-
-// // listening port
-// const PORT=process.env.PORT||3000;
-// app.listen(PORT,()=>{
-//     console.log(`app is live at ${PORT}`);
-// });
-
 require('dotenv').config();
 
 const express = require('express');
